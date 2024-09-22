8126669701a2a4e684e85f4200e62d640afb5321
jackorear <jorear@crimson.ua.edu>
2024-09-22 17:25:33 -0500

    Updating right wall too far away spees

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -176,8 +176,8 @@ class RandomWalk(Node):
         elif right_lidar_min > WALL_DISTANCE + LIDAR_ERROR and right_lidar_min < 3 * (WALL_DISTANCE + LIDAR_ERROR):
             # Too far from the right wall, turn right slightly
             self.get_logger().info('Too far from right wall, turning right.')
-            self.cmd.angular.z = -ANGULAR_VEL * 0.25  # Turn slightly right
-            self.cmd.linear.x = LINEAR_VEL * 0.75    # Slow forward movement
+            self.cmd.angular.z = -ANGULAR_VEL * .5  # Turn slightly right
+            self.cmd.linear.x = LINEAR_VEL * 0.5   # Slow forward movement
         else:
             # Maintain a straight path
             self.get_logger().info('Maintaining straight path.')
