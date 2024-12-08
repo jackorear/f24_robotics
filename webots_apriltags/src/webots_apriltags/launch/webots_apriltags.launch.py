25ad62ee96e2ba8a5a853137091664c7d0f2820f
jackorear <jorear@crimson.ua.edu>
2024-12-08 15:39:48 -0600

    Update webots_apriltags.launch.py

--- webots_apriltags/src/webots_apriltags/launch/webots_apriltags.launch.py
+++ webots_apriltags/src/webots_apriltags/launch/webots_apriltags.launch.py
@@ -99,11 +99,7 @@ def generate_launch_description():
         nodes_to_start= ros_control_spawners
     )
     
-    rviz_config_dir = os.path.join(get_package_share_directory('webots_apriltags'),
-                                   'rviz', 'turtlebot3_apriltags.rviz')
-
-
-
+    
     return LaunchDescription([
         DeclareLaunchArgument(
             'world',
@@ -132,12 +128,4 @@ def generate_launch_description():
                     launch.actions.EmitEvent(event=launch.events.Shutdown())
                 ],
             )
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
-    ])
+        )
