2a146134d6447b174507a0d02a07051338aa6a25
jackorear <jorear@crimson.ua.edu>
2024-12-08 15:53:05 -0600

    Update webots_apriltags.launch.py

--- webots_apriltags/src/webots_apriltags/launch/webots_apriltags.launch.py
+++ webots_apriltags/src/webots_apriltags/launch/webots_apriltags.launch.py
@@ -98,6 +98,11 @@ def generate_launch_description():
         target_driver=turtlebot_driver,
         nodes_to_start= ros_control_spawners
     )
+    
+    rviz_config_dir = os.path.join(get_package_share_directory('webots_apriltags'),
+                                   'rviz', 'turtlebot3_apriltags.rviz')
+
+
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -128,6 +133,11 @@ def generate_launch_description():
                 ],
             )
         ),
-
-        # RViz node removed
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
     ])
